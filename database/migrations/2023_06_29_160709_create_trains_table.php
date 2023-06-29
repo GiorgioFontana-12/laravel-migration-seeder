bca0b90d8884b9527387dcf1f257b01e8e559098
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-06-29 18:18:26 +0200

    feat migration table

---
+++ database/migrations/2023_06_29_160709_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company')->nullable();
+            $table->string('departure_station')->nullable();
+            $table->string('arrival_station')->nullable();
+            $table->string('departure_date')->nullable();
+            $table->char('departure_time', 5)->nullable();
+            $table->char('arrival_time', 5)->nullable();
+            $table->char('train_code', 30)->nullable();
+            $table->smallInteger('coaches_number')->nullable();
+            $table->boolean('on_time')->nullable();
+            $table->boolean('cancelled')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
